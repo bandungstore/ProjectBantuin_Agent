3bf7a25504ea0753cc6ea4ae2d994d8a5da19d71
Arda92 <azisramdhan92@gmail.com>
2017-03-14 13:01:46 +0700

    add activity prospek

---
+++ app/src/main/java/com/example/alfatih/project_01/Prospek.java
@@ -0,0 +1,13 @@
+package com.example.alfatih.project_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prospek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prospek);
+    }
+}
